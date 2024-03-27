e6e7bc09b06de21534d9b16a40992097611897a9
Fernando Almanza <almanzafernando@hotmail.com>
2024-03-26 21:39:49 -0500

    Inicio de la aplicación

---
+++ src/main/java/io/quind/technicaltesthexagonal/TechnicaltesthexagonalApplication.java
@@ -0,0 +1,13 @@
+package io.quind.technicaltesthexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicaltesthexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicaltesthexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/quind/technicaltesthexagonal/TechnicaltesthexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.quind.technicaltesthexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicaltesthexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
